deffea4bab96b1210c2566b52dc7cdd91bcc9e07
Jeff Swenson <jeffswenson@betterthannull.com>
2024-12-30 11:02:19 -0500

    crosscluster: rename StreamAddress
    There are two name changes included in this commit. The main change is
    "address" was renamed to "uri". In golang, "address" typically refers to
    a <hostname>:<port> pair like db.dns:26257. The "address" fields in the
    crosscluster package are mostly URIs. URIs include auth information and
    query parameters.
    
    Additionally, there are two families of URIs used by LDR and PCR. There
    is the URI supplied by the user and the URI extracted from the plan.
    These were renamed to "SourceConnURI" and "PartitionConnURI"
    respectively to make the destinction clear.
    
    Release Note: none

--- pkg/crosscluster/logical/create_logical_replication_stmt.go
+++ pkg/crosscluster/logical/create_logical_replication_stmt.go
@@ -160,12 +160,12 @@ func createLogicalReplicationStreamPlanHook(
 		// txn during statement execution.
 		p.InternalSQLTxn().Descriptors().ReleaseAll(ctx)
 
-		streamAddress := crosscluster.StreamAddress(from)
+		streamAddress := crosscluster.SourceClusterUri(from)
 		streamURL, err := streamAddress.URL()
 		if err != nil {
 			return err
 		}
-		streamAddress = crosscluster.StreamAddress(streamURL.String())
+		streamAddress = crosscluster.SourceClusterUri(streamURL.String())
 
 		cleanedURI, err := cloud.SanitizeExternalStorageURI(from, nil)
 		if err != nil {
@@ -259,7 +259,7 @@ func createLogicalReplicationStreamPlanHook(
 				SourceClusterID:           spec.SourceClusterID,
 				ReplicationStartTime:      replicationStartTime,
 				ReplicationPairs:          repPairs,
-				SourceClusterConnStr:      string(streamAddress),
+				SourceClusterConnUri:      string(streamAddress),
 				TableNames:                srcTableNames,
 				DefaultConflictResolution: defaultConflictResolution,
 				Discard:                   discard,
